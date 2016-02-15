1efd4f871f0060bcf8417313b39fc665479f0ac6
Nicholas and Will <student@epicodus.com>
2016-02-15 09:52:47 -0800

    Add test for changing e to 3.

--- Tests/LeetSpeakTest.cs
+++ Tests/LeetSpeakTest.cs
@@ -5,6 +5,20 @@ namespace LeetSpeakNS
 {
   public class LeetSpeakTest
   {
+    // English Spec List:
+      // The letter e should be replaced with 3
+      [Fact]
+      public void Translate_ForLetterE_ReplaceWith3()
+      {
+        Assert.Equal("3", LeetSpeak.Translate("e"));
+      }
+      // The letter o should be replaced with 0
+      // The capital letter I should be replaced with 1
+      // The letter t should be replaced with 7
+      // words in quotes should be ignored
+      // the letter s should be replaced with z, keeping case, unless at the front of the word
+      // the letter g should be replaced by 9
+    /**/
 
     /*
     [Fact]
--- objects/LeetSpeak.cs
+++ objects/LeetSpeak.cs
@@ -9,6 +9,19 @@ namespace LeetSpeakNS.Objects
 
     }
 
+    public static string Translate(string english)
+    {
+      char[] array = english.ToCharArray();
+      for (var i = 0; i < array.Length; i++)
+      {
+        if (array[i] == 'e')
+        {
+          array[i] = '3';
+        }
+      }
+      string result = string.Join("", array);
+      return result;
+    }
 
   }
 }
